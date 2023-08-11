d65dd0e29f456586b094f6625428aa10ef8a063c
Steven <bsteven48@gmail.com>
2023-08-11 11:45:45 -0400

    Updated lambda-fuinction module

--- lambda-function/main.tf
+++ lambda-function/main.tf
@@ -7,8 +7,12 @@ resource "aws_ecr_repository" "lambda_repository" {
 }
 
 resource "aws_lambda_function" "lambda_function" {
-  environment {
-    variables = var.env_vars
+
+  dynamic "environment" {
+    for_each = var.env_vars != {} ? [1] : []
+    content {
+        variables = var.env_vars
+    }
   }
   function_name   = "${var.product_name}-${var.function_name}"
   role            = var.iam_role_arn
