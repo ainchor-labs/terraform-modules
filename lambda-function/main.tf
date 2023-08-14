f3394df0b9ab7f040e12947e5732b52afae1bac1
Steven <bsteven48@gmail.com>
2023-08-14 13:24:25 -0400

    Updated Lambda module

--- lambda-function/main.tf
+++ lambda-function/main.tf
@@ -6,8 +6,34 @@ resource "aws_ecr_repository" "lambda_repository" {
   name = "${var.product_name}/${var.function_name}-function-repo"
 }
 
+# Use the AWS CLI to authenticate Docker to your ECR registry
+resource "null_resource" "docker_auth" {
+  provisioner "local-exec" {
+    command = "aws ecr get-login-password --region ${aws_ecr_repository.your_app_repository.region} | docker login --username AWS --password-stdin ${aws_ecr_repository.your_app_repository.repository_url}"
+  }
+}
+
+# Push the Docker image to the ECR repository
+resource "null_resource" "push_to_ecr" {
+  depends_on = [
+    null_resource.docker_auth,
+    aws_ecr_repository.lambda_repository
+  ]
+  
+  triggers = {
+    always_run = timestamp()
+  }
+
+  provisioner "local-exec" {
+    command = "docker tag your-app-image:latest ${aws_ecr_repository.your_app_repository.repository_url}:latest && docker push ${aws_ecr_repository.your_app_repository.repository_url}:latest"
+  }
+}
+
 resource "aws_lambda_function" "lambda_function" {
-  depends_on = [ aws_ecr_repository.lambda_repository ]
+  depends_on = [
+    aws_ecr_repository.lambda_repository,
+    aws.null_resource.push_to_ecr
+  ]
   environment {
     variables = var.env_vars
   }
