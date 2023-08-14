04f76f86211f00101a03fad20dcd0b3a96c9c4ed
Steven <bsteven48@gmail.com>
2023-08-14 14:03:54 -0400

    updated main

--- lambda-function/main.tf
+++ lambda-function/main.tf
@@ -15,7 +15,6 @@ resource "aws_lambda_function" "lambda_function" {
     variables = var.env_vars
   }
 
-
   function_name   = "${var.product_name}-${var.function_name}"
   role            = var.iam_role_arn
   memory_size     = var.memory
