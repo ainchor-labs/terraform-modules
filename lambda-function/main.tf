e02a4e8514c965364f60fa7bcfe388ca8db6d697
Steven <bsteven48@gmail.com>
2023-08-15 11:56:06 -0400

    ytjd

--- lambda-function/main.tf
+++ lambda-function/main.tf
@@ -2,10 +2,6 @@ provider "aws" {
 	region = "us-east-1"
 }
 
-# resource "aws_ecr_repository" "lambda_repository" {
-#   name = "${var.product_name}/${var.function_name}"
-# }
-
 resource "aws_lambda_function" "lambda_function" {
   environment {
     variables = var.env_vars
