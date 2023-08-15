ac8bec89101be35d56259ac12138cd01b9004d1d
Steven <bsteven48@gmail.com>
2023-08-15 10:07:53 -0400

    asdghasdgf

--- lambda-function/main.tf
+++ lambda-function/main.tf
@@ -1,6 +1,6 @@
-provider "aws" { 
-	region = "us-east-1"
-}
+# provider "aws" { 
+# 	region = "us-east-1"
+# }
 
 resource "aws_ecr_repository" "lambda_repository" {
   name = "${var.product_name}/${var.function_name}-function-repo"
