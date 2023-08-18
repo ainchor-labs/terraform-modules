c3141ede1c7d56d7a5d40b273ba8562d62a574e5
Ainchor Labs <steven@ainchor.co>
2023-08-18 11:22:28 -0400

    gdasgsa

--- iam/main.tf
+++ iam/main.tf
@@ -3,14 +3,14 @@ provider "aws" {
 }
 
 resource "aws_iam_role" "iam_role" {
-    name = var.name
+    name = "${var.product}_${var.name}"
     assume_role_policy = jsonencode({
         Version = "2012-10-17",
         Statement = [{
             Action = "sts:AssumeRole",
             Effect = "Allow",
             Principal = {
-                Service = "${vars.assume_role}.amazonaws.com"
+                Service = "${var.assume_role}.amazonaws.com"
             }
         }]}
     )
@@ -29,5 +29,5 @@ resource "aws_iam_policy_attachment" "policy_attachments" {
 
   name       = "${each.key}-attachment"
   policy_arn = aws_iam_policy.each.key_policy.arn
-  roles      = [aws_iam_role.role.name]
+  roles      = [aws_iam_role.iam_role.name]
 }
