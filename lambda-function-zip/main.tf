1943d9e0871ff67413cc1d5beaac7865c362d192
Ainchor Labs <steven.burgess@pluto.tv>
2023-08-23 11:09:53 -0400

    Updated to include zip updates

--- lambda-function-zip/main.tf
+++ lambda-function-zip/main.tf
@@ -6,12 +6,12 @@ resource "aws_lambda_function" "lambda_function" {
   environment {
     variables = var.env_vars
   }
-  s3_bucket       = "ainchor-news-lambda-zip-bucket"
-  s3_key          = var.filename
-  function_name   = "${var.product_name}-${var.function_name}"
-  handler         = "app.handler"
-  role            = var.iam_role_arn
-  memory_size     = var.memory
-  runtime         = var.runtime
-  timeout         = var.timeout
+  filename         = var.filename
+  function_name    = "${var.product_name}-${var.function_name}"
+  handler          = "app.handler"
+  role             = var.iam_role_arn
+  memory_size      = var.memory
+  runtime          = var.runtime
+  timeout          = var.timeout
+  source_code_hash = base64sha256(file(var.filename))
 }
