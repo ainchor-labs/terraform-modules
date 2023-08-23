6d67ddc2bd05115c38724bb7edc3454627276213
Ainchor Labs <steven.burgess@pluto.tv>
2023-08-23 11:05:11 -0400

    Updated to use s3 bucket

--- lambda-function-zip/main.tf
+++ lambda-function-zip/main.tf
@@ -6,7 +6,8 @@ resource "aws_lambda_function" "lambda_function" {
   environment {
     variables = var.env_vars
   }
-  filename        = var.filename
+  s3_bucket       = "ainchor-news-lambda-zip-bucket"
+  s3_key          = var.filename
   function_name   = "${var.product_name}-${var.function_name}"
   handler         = "app.handler"
   role            = var.iam_role_arn
